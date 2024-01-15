1bcb543e7ea0e100a4edc4989fea97f7c703e2aa
Satyam Mishra <satyamsumit8750@gmail.com>
2024-01-16 00:39:48 +0530

    wokring on home page 2 section completed

--- 12project/src/components/Home.jsx
+++ 12project/src/components/Home.jsx
@@ -1,13 +1,45 @@
 import React from "react";
+import vg from "../assets/2.webp";
 
 const Home = () => {
   return (
-    <div className="home">
-      <main>
-        <h1>TeachyStar</h1>
-        <p>Solution to all your problems </p>
-      </main>
-    </div>
+    <>
+      {/* 1 */}
+      <div className="home">
+        <main>
+          <h1>TeachyStar</h1>
+          <p>Solution to all your problems </p>
+        </main>
+      </div>
+
+      {/* 2 */}
+      <div className="home2">
+        <img src={vg} alt="Graphics" />
+        <div>
+          <p>
+            We are your one and only solution to the tech problems you face
+            everyday. We are leading tech company whose aim is to increase the
+            problem solving ability in children
+          </p>
+        </div>
+      </div>
+
+      {/* 3 */}
+      <div className="home3">
+        <div>
+          <h1>Who we are?</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto
+            sequi quibusdam beatae architecto ea laboriosam delectus nihil quis
+            quo explicabo aspernatur, dolores velit perspiciatis quaerat
+            deserunt? Nihil nam sequi incidunt? Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Ratione nisi dicta, magni explicabo
+            excepturi quas. Ipsa quod minima omnis sequi cumque possimus
+            repellendus, alias asperiores molestiae ipsum quaerat iure laborum?
+          </p>
+        </div>
+      </div>
+    </>
   );
 };
 
