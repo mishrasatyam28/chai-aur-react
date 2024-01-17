ff30595a943e65ad196a956000b5bfcdac783d06
Satyam Mishra <satyamsumit8750@gmail.com>
2024-01-17 23:05:16 +0530

    services

--- 12project/src/App.jsx
+++ 12project/src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
 
 import "./styles/App.scss";
 import "./styles/header.scss";
 import "./styles/home.scss";
-import Footer from "./components/Footer";
+import "./styles/footer.scss";
+import "./styles/contact.scss";
+import "./styles/services.scss";
 
 function App() {
   return (
@@ -13,6 +18,8 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
       <Footer />
     </Router>
